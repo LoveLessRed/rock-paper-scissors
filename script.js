fae31d4e8be3c59a7aad48f33842d0d59ce381be
LoveLess <kote.eas@gmail.com>
2022-06-25 10:30:34 +0200

    Allow for the user to choose between ROCK, PAPER and SCISSORS against a random selection

--- script.js
+++ script.js
@@ -2,17 +2,43 @@ function computerPlay() {
     return Math.floor(Math.random() * 3) + 1
 }
 
+// In the function playRound(playerSelection, computerSelection) 1 stands for ROCK, 2 for PAPER and 3 for SCISSORS!
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "ROCK" && computerSelection == 1) {
+    if (playerSelection.toUpperCase() == "ROCK" && computerSelection == 1) {
         result = "Tie";
         return result;
     }
-    else if (playerSelection == "ROCK" && computerSelection == 2) {
+    else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == 2) {
         result = "You lose! Paper beats Rock";
         return result;
     }
-    else if (playerSelection == "ROCK" && computerSelection == 3) {
+    else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == 3) {
         result = "You win! Rock beats Scissors";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == 1) {
+        result = "You win! Paper beats Rock";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == 2) {
+        result = "Tie";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == 3) {
+        result = "You lose! Scissors beats Paper";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == 1) {
+        result = "You lose! Rock beats Scissors";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == 2) {
+        result = "You win! Scissors beats Paper";
+        return result;
+    }
+    else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == 3) {
+        result = "Tie";
         return result;
     }
     else {
@@ -21,6 +47,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "ROCK";
-const computerSelection = computerPlay();
+let playerSelection = prompt("Choose: ROCK, PAPER or SCISSORS");
+let computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
